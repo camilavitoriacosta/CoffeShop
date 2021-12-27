e9794150c3c7fcecec2b3ee5d4bc120c681c29f7
Camila Vitoria <camilavitoria.c.costa@gmail.com>
2021-12-27 17:19:32 -0400

    Inserindo imagem no produto

--- produtos_app/models.py
+++ produtos_app/models.py
@@ -5,5 +5,7 @@ class Produto(models.Model):
     descricao = models.CharField(max_length=250)
     preco = models.FloatField()
     categoria = models.CharField(max_length=50)
+
+    foto_produto = models.ImageField(upload_to='fotos/', blank=True)
     
 
