b0e864016ff513023accb6ecd2e3faa53e38a9dc
Camila Vitoria <camilavitoria.c.costa@gmail.com>
2021-12-27 16:15:55 -0400

    Criando Produtos

--- produtos_app/urls.py
+++ produtos_app/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
-urlpatterns = {
-    path('', views.pagina_inicial, name='pagina_inicial')
-}
+urlpatterns = [
+    path('', views.pagina_inicial,name='pagina_inicial'),
+    path('catalogo_produtos/coffe', views.catalogo_produtos_coffe, name='catalogo_produtos_coffe'),
+    path('catalogo_produtos/tea', views.catalogo_produtos_tea, name='catalogo_produtos_tea'),
+    path('catalogo_produtos/smoothie', views.catalogo_produtos_smoothie, name='catalogo_produtos_smoothie')
+]
--- produtos_app/views.py
+++ produtos_app/views.py
@@ -1,4 +1,43 @@
 from django.shortcuts import render
+from .models import *
 
 def pagina_inicial(requisicao):
     return render(requisicao, 'pagina_inicial.html')
+
+def catalogo_produtos_coffe(requisicao):
+    produtos = filtrar_produto_por_categoria("coffe")    
+    dados = {
+        'produtos': produtos
+    }
+    
+    return render(requisicao, 'catalogo_produtos.html', dados)
+
+def catalogo_produtos_tea(requisicao):
+    produtos = filtrar_produto_por_categoria("tea")    
+    dados = {
+        'produtos': produtos
+    }
+    
+    return render(requisicao, 'catalogo_produtos.html', dados)
+
+def catalogo_produtos_smoothie(requisicao):
+    produtos = filtrar_produto_por_categoria("smoothie")    
+    dados = {
+        'produtos': produtos
+    }
+    return render(requisicao, 'catalogo_produtos.html', dados)
+
+def filtrar_produto_por_categoria(categoria_produto):
+    return Produto.objects.filter(categoria=categoria_produto)
+    
+def buscar_produto(requisicao):
+    lista_produtos = Produto.objects.all()
+
+    if 'buscar' in requisicao.GET:
+        texto_busca = requisicao.GET['buscar']
+        lista_produtos = lista_produtos.filter(nome_produto__icontains=texto_busca)
+        dados = {
+            'produtos': lista_produtos
+        }
+
+    return render(requisicao, 'catalogo_produtos.html', dados)
