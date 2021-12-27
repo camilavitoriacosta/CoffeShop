27dd04134795d3a788e2835e9a9d4928b89c6100
Camila Vitoria <camilavitoria.c.costa@gmail.com>
2021-12-27 18:06:11 -0400

    Adicionando cadastro de usuarios

--- usuarios_app/views.py
+++ usuarios_app/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import redirect, render
+from .models import *
+from django.contrib.auth.models import User
 
 # Create your views here.
 def cadastro_usuario(requisicao):
     if requisicao.method == 'POST':
+        email = requisicao.POST['email']
+        senha = requisicao.POST['senha']
+        senha_confirmacao = requisicao.POST['senha_confirmacao']
+
+        # Validar campos
+        valida_campos(email,senha,senha_confirmacao)
+        #Mostrar mensagem para o usuario de sucesso ou erro
         print('Usuario criado com sucesso')
         return redirect('login')
 
@@ -13,3 +22,33 @@ def login(requisicao):
 
 def logout(requisicao):
     pass
+
+
+def valida_campos(email, senha, senha_confirmacao):
+    if not email.strip():
+        print('O campo email não pode ficar em branco')
+        return redirect('cadastro_usuario')
+    if not senha.strip():
+        print('O campo senha não pode ficar em branco')
+        return redirect('cadastro_usuario')
+    if not senha_confirmacao.strip():
+        print('O campo de confirmação de senha não pode ficar em branco')
+        return redirect('cadastro_usuario')
+    if senha != senha_confirmacao:
+        print('Senhas não conferem')
+        return redirect('cadastro_usuario')
+    
+    # Verificar se o usuario já existe
+    if User.objects.filter(email=email).exists():
+        print('Usuario já cadastrado')
+        return redirect('cadastro_usuario')
+    else:
+        # Inserir usuario no banco
+        cadastrar_Usuario(email, senha)
+        return redirect('login')
+
+        
+
+def cadastrar_Usuario(email, senha):
+    usuario = Usuario(email=email, senha=senha)
+    usuario.save()
