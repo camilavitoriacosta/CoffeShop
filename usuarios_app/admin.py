9e9c405bc9f9ac8ba363433b875393239e476ae4
Camila Vitoria <camilavitoria.c.costa@gmail.com>
2021-12-28 12:03:00 -0400

    adicionando usuarios no admin

--- usuarios_app/admin.py
+++ usuarios_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+class ListandoUsuarios(admin.ModelAdmin):
+    list_display = ('id', 'email')
+    list_display_links = ('id', 'email')
+
+admin.site.register(Usuario, ListandoUsuarios)
